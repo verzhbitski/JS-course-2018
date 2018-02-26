00106b04c7e313a34aad9a836f220eed580a111d
verzhbitski <verzhbitski@gmail.com>
2018-02-26 19:54:16 +0300

    added task1

---
+++ task1/task1.js
@@ -0,0 +1,13 @@
+function bin_to_dec(binary) {
+    for (let char of binary) {
+        if (char !== '0' && char !== '1') {
+            return undefined;
+        }
+    }
+
+    return binary.split('').reverse().reduce(function (acc, value, index) {
+        return acc + value * Math.pow(2, index);
+    }, 0);
+}
+
+console.log(bin_to_dec('111'))
